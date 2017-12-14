2351211b5f0599f081a0b93df576ebfaf6af47f3
Quincy <291079050@qq.com>
2017-12-14 19:52:28 +0800

    初始化项目

---
+++ web01/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Web01Config(AppConfig):
+    name = 'web01'
---
+++ web02/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Web02Config(AppConfig):
+    name = 'web02'
