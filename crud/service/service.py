676666a9bf73772773ec27a8728b047cd26db4b0
Quincy <291079050@qq.com>
2017-12-25 08:31:40 +0800

    完善添加页面，优化代码，主要针对反向生成url进进行封装

--- crud/service/service.py
+++ crud/service/service.py
@@ -1,6 +1,7 @@
+from django.forms import ModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.urls import path
+from django.shortcuts import render, redirect
+from django.urls import path, reverse
 from django.utils.safestring import mark_safe
 
 
@@ -26,6 +27,20 @@ class CrudConfig:
             result.append(CrudConfig.delete)
         return result
 
+    # 添加按钮显示与隐藏
+    show_add_btn = False
+
+    def get_show_add_btn(self):
+        return self.show_add_btn
+
+    def get_add_btn(self):
+        """
+        给添加按钮赋值url
+        :return:
+        """
+        # /crm/web02/userinfo/add
+        return mark_safe('<a href=%s class="btn btn-success">添加</a>' % self.get_add_url())
+
     @property
     def urls(self):
         return self.get_urls()
@@ -38,7 +53,15 @@ class CrudConfig:
             path('<int:obj_id>/change/', self.change_view, name='%s_%s_change' % info),
             path('<int:obj_id>/delete/', self.delete_view, name='%s_%s_delete' % info),
         ]
+        urlpatterns.extend(self.extra_url())
         return urlpatterns
+
+    def extra_url(self):
+        """
+        处理增删改查以外的url,即自定义url
+        :return:
+        """
+        return []
 
     def changelist_view(self, request):
         """
@@ -70,9 +93,31 @@ class CrudConfig:
                 temp.append(val)
             new_data_list.append(temp)
 
-        return render(request, 'change_list.html', {'data_list': new_data_list, 'header_list': header_list})
+        return render(request, 'change_list.html',
+                      {'data_list': new_data_list, 'header_list': header_list, 'add_btn': self.get_add_btn})
 
     def add_view(self, request):
+        """
+        添加视图处理
+        :param request:
+        :return:
+        """
+
+        class MyForm(ModelForm):
+            class Meta:
+                model = self.model
+                fields = '__all__'
+
+        if request.method == 'GET':
+            form = MyForm()
+            return render(request, 'add_view.html', {'form': form})
+        elif request.method == 'POST':
+            form = MyForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect(self.get_show_url())
+            return render(request, 'add_view.html', {'form': form})
+
         return HttpResponse('添加')
 
     def change_view(self, request, obj_id):
@@ -83,19 +128,75 @@ class CrudConfig:
 
     # 自定义checkbox,edit，delete
     def checkbox(self, obj=None, is_header=False):
+        """
+        页面checkbox选项
+        :param obj:
+        :param is_header:
+        :return:
+        """
         if is_header:
             return '选择'
         return mark_safe('<input type="checkbox" value=%s>' % obj.id)
 
     def edit(self, obj=None, is_header=False):
+        """
+        页面编辑选项
+        :param obj:
+        :param is_header:
+        :return:
+        """
         if is_header:
             return '操作0'
-        return mark_safe('<a href="/edit/%s/">编辑</a>' % obj.id)
+        return mark_safe('<a href=%s>编辑</a>' % self.get_change_url(obj.id))
 
     def delete(self, obj=None, is_header=False):
+        """
+        页面删除选项
+        :param obj:
+        :param is_header:
+        :return:
+        """
         if is_header:
             return '操作1'
-        return mark_safe('<a href="/delete/%s/">删除</a>' % obj.id)
+        return mark_safe('<a href={0}>删除</a>'.format(self.get_delete_url(obj.id)))
+
+    def get_add_url(self):
+        """
+        反向生成添加url
+        :return:
+        """
+        info = self.model._meta.app_label, self.model._meta.model_name
+        url = reverse('%s_%s_add' % info)
+        return url
+
+    def get_show_url(self):
+        """
+        反向生成列表url
+        :return:
+        """
+        info = self.model._meta.app_label, self.model._meta.model_name
+        url = reverse('%s_%s_changelist' % info)
+        return url
+
+    def get_change_url(self, oid):
+        """
+        反向生成编辑url
+        :return:
+        """
+        info = self.model._meta.app_label, self.model._meta.model_name
+        # 反向生成url需要参数时用args
+        url = reverse('%s_%s_change' % info, args=(oid,))
+        return url
+
+    def get_delete_url(self, oid):
+        """
+        反向生成删除url
+        :return:
+        """
+        info = self.model._meta.app_label, self.model._meta.model_name
+        # 反向生成url需要参数时用args
+        url = reverse('%s_%s_delete' % info, args=(oid,))
+        return url
 
 
 class CrudSite:
--- web02/crud.py
+++ web02/crud.py
@@ -1,7 +1,6 @@
 """
 相当于admin文件
 """
-from django.utils.safestring import mark_safe
 
 from crud.service import service
 from crud.service.service import CrudConfig
@@ -13,21 +12,6 @@ class UserInfoConfig(CrudConfig):
     继承CrudConfig，派生新的功能
     """
 
-    # def checkbox(self, obj=None, is_header=False):
-    #     if is_header:
-    #         return '选择'
-    #     return mark_safe('<input type="checkbox" value=%s>' % obj.id)
-
-    # def edit(self, obj=None, is_header=False):
-    #     if is_header:
-    #         return '操作'
-    #     return mark_safe('<a href="/edit/%s/">编辑</a>' % obj.id)
-    #
-    # def delete(self, obj=None, is_header=False):
-    #     if is_header:
-    #         return '删除'
-    #     return mark_safe('<a href="/delete/%s/">删除</a>' % obj.id)
-    # list_display = [checkbox, 'id', 'name', 'pwd', edit]
     list_display = ['id', 'name', 'pwd']
 
 
